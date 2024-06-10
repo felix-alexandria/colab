017c52830a134b7f9d0574deeacac1078a546e90
felix-alexandria <jesmiah0603@gmail.com>
2024-06-10 12:53:51 +0100

    statr again

--- app/group1/page.tsx
+++ app/group1/page.tsx
@@ -4,6 +4,7 @@ const page = () => {
     return (
       <div className="w-full h-full bg-red-900 flex justify-center items-center">
         <div className="w-[95%] h-[95%] bg-white ">Start Now</div>
+        <div>Start working</div>
       </div>
     );
 };
