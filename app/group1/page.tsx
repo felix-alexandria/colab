4f40ce100210b109c693ecf3fb3b1239805d02ab
felix-alexandria <jesmiah0603@gmail.com>
2024-06-10 12:40:30 +0100

    second commit

--- app/group1/page.tsx
+++ app/group1/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+    return (
+      <div className="w-full h-full bg-red-900 flex justify-center items-center">
+        <div className="w-[95%] h-[95%] bg-white ">Start Now</div>
+      </div>
+    );
 };
 
 export default page;
